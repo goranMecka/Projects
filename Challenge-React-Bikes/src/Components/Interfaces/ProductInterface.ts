bfcba287b3f265d1395dab4938292b2bd6ddfb28
Goran Gelevski <gorangelevskigti@gmail.com>
2024-06-22 11:15:12 +0200

    added gazoom project

--- Challenge-React-Bikes/src/Components/Interfaces/ProductInterface.ts
+++
@@ -1,9 +0,0 @@
- interface Product {
-    name: string;
-    price: number;
-    image: string;
-    gender: string;
-    brand: string
-  }
-
-  export default Product
--- Challenge-React-Bikes/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { ServerHMRConnector, defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-
